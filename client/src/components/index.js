19cfe8957477567f64313162838aaca3e6a490ac
dauvanmuoi <dauvanmuoi@gmail.com>
2022-02-07 10:10:12 +0700

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,28 @@
+import Logo from "./Logo";
+import FormRow from "./FormRow";
+import Alert from "./Alert";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
+import FormRowSelect from "./FormRowSelect";
+import JobsContainer from "./JobsContainer";
+import SearchContainer from "./SearchContainer";
+import Loading from "./Loading";
+import StatsItem from "./StatsItem";
+import ChartsContainer from "./ChartsContainer";
+import StatsContainer from "./StatsContainer";
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FormRowSelect,
+  JobsContainer,
+  SearchContainer,
+  Loading,
+  StatsItem,
+  ChartsContainer,
+  StatsContainer,
+};
---
+++ server.js
@@ -0,0 +1,72 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import "express-async-errors";
+import morgan from "morgan";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
+// db
+import connectDB from "./db/connect.js";
+
+// import routes
+import authRouter from "./routes/authRoutes.js";
+import jobRouter from "./routes/jobsRoutes.js";
+// middleware
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+import authenticateUser from "./middleware/auth.js";
+
+// morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.json());
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/job", authenticateUser, jobRouter);
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+//app use
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
+// setting server
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
